e8e0b6bc3a7e7ebfaa43ac776eb69078b2a807cd
GMTAccount <guillaume.tadjer@edu.devinci.fr>
2020-12-16 23:25:25 +0100

    Update : ajout des tests unitaires

---
+++ source/repos/Parce que c'est notre projet/Parce que c'est notre projet/IA.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Parce_que_c_est_notre_projet
+{
+    class IA
+    {
+
+    }
+}
--- source/repos/Parce que c'est notre projet/Parce que c'est notre projet/README.md
+++ source/repos/Parce que c'est notre projet/Parce que c'est notre projet/README.md
@@ -1,3 +1,4 @@
 Reste à faire : 
 - Réfléchir à l'exo 6 (bonus)
 - Améliorer la User Experience
+- Faire les tests unitaires
