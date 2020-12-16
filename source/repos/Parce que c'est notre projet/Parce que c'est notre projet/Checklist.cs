8edc08714b6ce00126529eccf8e382a454f09f5e
GMTAccount <guillaume.tadjer@edu.devinci.fr>
2020-12-16 22:49:13 +0100

    La checklist est dans README.md désormais

--- source/repos/Parce que c'est notre projet/Parce que c'est notre projet/Checklist.cs
+++ source/repos/Parce que c'est notre projet/Parce que c'est notre projet/Checklist.cs
@@ -7,6 +7,7 @@ namespace Parce_que_c_est_notre_projet
     class Checklist
     {
         /*
+         * Cette checklist n'est plus actualisée, merci de passer par README.md
             Reste à faire : 
                 - Finaliser la méthode Test_Plateau (erreur au niveau du else/if ou de else)
                 - Faire l'exo 6
--- source/repos/Parce que c'est notre projet/Parce que c'est notre projet/README.md
+++ source/repos/Parce que c'est notre projet/Parce que c'est notre projet/README.md
@@ -1,3 +1,3 @@
 Reste à faire : 
-- Finaliser la méthode Test_Plateau (au niveau du else if et else)
 - Réfléchir à l'exo 6 (bonus)
+- Améliorer la User Experience
