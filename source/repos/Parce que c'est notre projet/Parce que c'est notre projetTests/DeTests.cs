cf7440003cd79cff3ef6325efb21a6b3903cd46e
GMTAccount <guillaume.tadjer@edu.devinci.fr>
2020-12-17 16:18:32 +0100

    Tests Joueur et De

---
+++ source/repos/Parce que c'est notre projet/Parce que c'est notre projetTests/DeTests.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Parce_que_c_est_notre_projet;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Parce_que_c_est_notre_projet.Tests
+{
+    [TestClass()]
+    public class DeTests
+    {
+        [TestMethod()]
+        public void DeTestMauvaisNombreFaces()
+        {
+            De test = new De("A; B; C", 6);
+            Assert.AreEqual(3, test.Des.Length);
+        }
+        [TestMethod()]
+        public void toStringTest()
+        {
+            De test = new De("A; B; C", 3);
+            Assert.AreEqual("A B C", test.toString());
+        }
+    }
+}
--- source/repos/Parce que c'est notre projet/Parce que c'est notre projetTests/JoueurTests.cs
+++ source/repos/Parce que c'est notre projet/Parce que c'est notre projetTests/JoueurTests.cs
@@ -8,29 +8,29 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace Parce_que_c_est_notre_projet.Tests
 {
-    [TestClass]
+    [TestClass()]
     public class JoueurTests
     {
-        [TestMethod]
+        [TestMethod()]
         public void JoueurTestScore()
         {
             Joueur joueur = new Joueur("Test");
             Assert.AreEqual(0, joueur.Score);
         }
-        [TestMethod]
+        [TestMethod()]
         public void JoueurTestNom()
         {
             Joueur joueur = new Joueur("Test");
             Assert.AreEqual("Test", joueur.Nom);
         }
-        [TestMethod]
+        [TestMethod()]
         public void JoueurTestNull()
         {
             Joueur joueur = new Joueur("Test");
-            Assert.AreEqual(null, joueur.MotTrouve);
+            Assert.AreEqual(0, joueur.MotTrouve.Count);
         }
 
-        [TestMethod]
+        [TestMethod()]
         public void ContainTestVide()
         {
             Joueur joueur = new Joueur("Test");
@@ -52,7 +52,7 @@ namespace Parce_que_c_est_notre_projet.Tests
         }
 
         [TestMethod()]
-        public void toStringTest()
+        public void ToStringTest()
         {
             Joueur joueur = new Joueur("Test");
             string s = joueur.toString();
