9ce46277899e0c3e8e690e9ba8ddfb64c2dee154
GMTAccount <guillaume.tadjer@edu.devinci.fr>
2020-12-17 15:14:49 +0100

    Version 1.8 : revue du fichier destatus
    git status

---
+++ source/repos/Parce que c'est notre projet/UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Parce_que_c_est_notre_projet
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            // Tests liés à Joueur
+
+            // Tests liés à De
+
+            // Tests liés à Dictionnaire
+
+            // Tests liés à Plateau
+
+            // Tests liés à IA
+
+        }
+    }
+}
