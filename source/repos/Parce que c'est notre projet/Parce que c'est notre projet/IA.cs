b3482a4475dba3941aa4ac83415ec44969d70a08
GMTAccount <guillaume.tadjer@edu.devinci.fr>
2020-12-17 18:18:09 +0100

    Version 2.0 : les IA nous envahissent git statusgit status

--- source/repos/Parce que c'est notre projet/Parce que c'est notre projet/IA.cs
+++ source/repos/Parce que c'est notre projet/Parce que c'est notre projet/IA.cs
@@ -10,7 +10,11 @@ namespace Parce_que_c_est_notre_projet
         private List<string> motsTrouves;
         private List<string> dictionnaire;
         private string[,] valeurSup;
-
+        /// <summary>
+        /// Constructeur d'un joueur de type IA
+        /// </summary>
+        /// <param name="dico">Tableau de dictionnaire</param>
+        /// <param name="langue">Langue souhaitée</param>
         public IA(Dictionnaire[] dico, int langue)
         {
             this.dictionnaire = new List<string>();
@@ -28,8 +32,12 @@ namespace Parce_que_c_est_notre_projet
             jeu.Monplateau.MelangeValeurs();
             this.valeurSup = jeu.Monplateau.ValeurSup;
         }
+        /// <summary>
+        /// Recherche de TOUS les mots du dictionnaire dans la grille actuelle
+        /// </summary>
         public void RechercheMots()
         {
+            Console.WriteLine(this.dictionnaire.Count);
             Console.WriteLine(this.jeu.Monplateau.ToString());
             Console.WriteLine();
             for (int i = 0; i < this.dictionnaire.Count; i++)
@@ -41,112 +49,26 @@ namespace Parce_que_c_est_notre_projet
                 }
             }
         }
-        public bool Test_Plateau(string mot, int posimot, List<int> positionMot = null)
+        /// <summary>
+        /// Retour d'un mot trouvé de façon aléatoire (pour donner une chance aux joueurs en chair et en os)
+        /// </summary>
+        /// <param name="motsDejaDonnes">Liste des mots déjà données par l'IA durant la partie</param>
+        /// <returns>Mot qu'elle a choisi de donner à ce tour</returns>
+        public string DistributionMots(List<string> motsDejaDonnes)
         {
-            
-            bool estPresent = false;
-            if (mot != null && mot.Length != 0) // On vérifie si le mot peut être analysé
+            string retour = "";
+            if(this.motsTrouves != null && this.motsTrouves.Count != 0 && this.motsTrouves.Count > motsDejaDonnes.Count)
             {
-                // Analyse pour la première position
-                // On recherche d'abord si la première lettre du mot est bien dans le plateau
-                if (posimot == 0)
+                int posimot = 0;
+                Random r = new Random();
+                do
                 {
-                    // Parcours de toutes les cases du plateau pour trouver la première lettre du mot
-                    for (int i = 0; i < this.valeurSup.GetLength(0) && !estPresent; i++)
-                    {
-                        for (int j = 0; j < this.valeurSup.GetLength(1) && !estPresent; j++)
-                        {
-                            if (this.valeurSup[i, j] == Convert.ToString(mot[0]))
-                            {
-                                // La première lettre du mot est dans le plateau, à la case [i, j]
-                                positionMot = new List<int>(); // On initialise la liste comportant les coordonnées des cases déjà étudiées
-                                // On ajoute à cette liste la position actuelle (d'abord i, puis j)
-                                positionMot.Add(i);
-                                positionMot.Add(j);
-                                estPresent = Test_Plateau(mot, posimot + 1, positionMot); // On va ensuite chercher les autres lettres du mot
-                                if (!estPresent) // Si la recherche de la ligne ci-dessus a échoué, on retire les positions de la liste
-                                {
-                                    positionMot.RemoveRange(0, positionMot.Count);
-                                }
-                                else
-                                {
-                                    // Si on a trouvé le mot, on retourne directement la valeur true
-                                    return estPresent;
-                                }
-                            }
-                        }
-                    }
+                    posimot = r.Next(0, this.motsTrouves.Count);
+                    retour = this.motsTrouves[posimot];
                 }
-                // On recherche ensuite toutes les autres lettres (dernière lettre exclue du else if)
-                else if (posimot < mot.Length - 1)
-                {
-                    // Analyse des cases voisines à celle que l'on a trouvée précédemment (et qui nous est communiquée par la liste positionMot)
-                    for (int i = positionMot[positionMot.Count - 2] - 1; i <= positionMot[positionMot.Count - 2] + 1 && !estPresent; i++)
-                    {
-                        for (int j = positionMot[positionMot.Count - 1] - 1; j <= positionMot[positionMot.Count - 1] + 1 && !estPresent; j++)
-                        {
-                            if (((i >= 0 && i < this.valeurSup.GetLength(0)) && (j >= 0 && j < this.valeurSup.GetLength(1))) && (i != positionMot[positionMot.Count - 2] || j != positionMot[positionMot.Count - 1]) && this.valeurSup[i, j] == Convert.ToString(mot[posimot]))
-                            {
-                                // On a trouvé une lettre qui est égale à la posimot-ième lettre de notre mot, et qui n'est pas sur la case sur laquelle on est centré
-                                // (On vérifie aussi si les coordonnées ne sont pas out of range)
-                                bool test = true;
-                                for (int k = 0; (k < positionMot.Count - 1 && test); k += 2) // On teste si la case correspondant aux conditions ci-dessus n'est pas déjà dans le chemin en cours d'étude (éviter les retours en arrière et doublons)
-                                {
-                                    if (i == positionMot[k] && j == positionMot[k + 1])
-                                    {
-                                        test = false;
-                                    }
-                                }
-                                if (test) // La lettre n'appartient pas à la liste des cases du chemin en cours, on peut donc aller travailler dessus
-                                {
-                                    // On ajoute à cette liste la position actuelle (d'abord i, puis j)
-                                    positionMot.Add(i);
-                                    positionMot.Add(j);
-                                    estPresent = Test_Plateau(mot, posimot + 1, positionMot); // On va ensuite chercher les autres lettres du mot
-                                    positionMot.RemoveAt(positionMot.Count - 1); // Par défaut, on retire ces positions de la liste (pour éviter des tests inutiles)
-                                    positionMot.RemoveAt(positionMot.Count - 1);
-                                }
-                                if (estPresent) // Si on a en retour true, on le retourne immédiatement, mettant fin à la récursivité
-                                {
-                                    return estPresent;
-                                }
-                            }
-                        }
-                    }
-                }
-                // On recherche enfin la dernière lettre
-                else
-                {
-                    // Analyse des cases voisines à celle que l'on a trouvée précédemment (et qui nous est communiquée par la liste positionMot)
-                    for (int i = positionMot[positionMot.Count - 2] - 1; i <= positionMot[positionMot.Count - 2] + 1 && !estPresent; i++)
-                    {
-                        for (int j = positionMot[positionMot.Count - 1] - 1; j <= positionMot[positionMot.Count - 1] + 1 && !estPresent; j++)
-                        {
-                            if (((i >= 0 && i < this.valeurSup.GetLength(0)) && (j >= 0 && j < this.valeurSup.GetLength(1))) && (i != positionMot[positionMot.Count - 2] || j != positionMot[positionMot.Count - 1]) && this.valeurSup[i, j] == Convert.ToString(mot[posimot]))
-                            {
-                                // On a trouvé une lettre qui est égale à la posimot-ième lettre de notre mot, et qui n'est pas sur la case sur laquelle on est centré
-                                // (On vérifie aussi si les coordonnées ne sont pas out of range)
-                                bool test = true;
-                                for (int k = 0; k < positionMot.Count - 1; k += 2) // On teste si la case correspondant aux conditions ci-dessus n'est pas déjà dans le chemin en cours d'étude (éviter les retours en arrière et doublons)
-                                {
-                                    if (i == positionMot[k] && j == positionMot[k + 1])
-                                    {
-                                        test = false;
-                                    }
-                                }
-                                if (test) // Si la dernière lettre est dans le voisinage de l'avant dernière et n'a pas été parcourue, on termine la récursivité, en retournant true
-                                {
-                                    return true;
-                                    break;
-                                }
-                            }
-                        }
-                    }
-                }
+                while (motsDejaDonnes.Contains(retour));
             }
-
-            return estPresent; // Dans le cas où l'on a pas trouvé le mot (ou il ne correspond pas aux critères de longueur), on retourne false par défaut
+            return retour;
         }
-        
     }
 }
--- source/repos/Parce que c'est notre projet/Parce que c'est notre projet/README.md
+++ source/repos/Parce que c'est notre projet/Parce que c'est notre projet/README.md
@@ -1,10 +1,9 @@
 Reste à faire (par nom de fichier) : 
 
 1 - Program.cs : 
-Revoir l'affichage
+Revoir le Main
 
 Idées pour la mise en place de IA.cs : 
 3 niveaux de difficulté (1 et 5/7/10)
-Mots choisis aléatoirement dans la liste
 Durée de "réflexion" aléatoire
 Découpage de 60 en 3 parties aléatoires (60 - trouvé et 0)
