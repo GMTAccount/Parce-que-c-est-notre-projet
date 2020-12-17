de8b15486c61032019e23c486774e6460ccfd99b
GMTAccount <guillaume.tadjer@edu.devinci.fr>
2020-12-17 15:57:16 +0100

    Version 1.8 : arrivée des tests

---
+++ source/repos/Parce que c'est notre projet/Parce que c'est notre projetTests/JoueurTests.cs
@@ -0,0 +1,62 @@
+﻿using Parce_que_c_est_notre_projet;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Parce_que_c_est_notre_projet.Tests
+{
+    [TestClass]
+    public class JoueurTests
+    {
+        [TestMethod]
+        public void JoueurTestScore()
+        {
+            Joueur joueur = new Joueur("Test");
+            Assert.AreEqual(0, joueur.Score);
+        }
+        [TestMethod]
+        public void JoueurTestNom()
+        {
+            Joueur joueur = new Joueur("Test");
+            Assert.AreEqual("Test", joueur.Nom);
+        }
+        [TestMethod]
+        public void JoueurTestNull()
+        {
+            Joueur joueur = new Joueur("Test");
+            Assert.AreEqual(null, joueur.MotTrouve);
+        }
+
+        [TestMethod]
+        public void ContainTestVide()
+        {
+            Joueur joueur = new Joueur("Test");
+            Assert.AreEqual(false, joueur.Contain("Mot"));
+        }
+        [TestMethod()]
+        public void ContainTestNon()
+        {
+            Joueur joueur = new Joueur("Test");
+            joueur.Add_Mot("Mot");
+            Assert.AreEqual(false, joueur.Contain("Motte"));
+        }
+        [TestMethod()]
+        public void Add_MotTest()
+        {
+            Joueur joueur = new Joueur("Test");
+            joueur.Add_Mot("Mot");
+            Assert.AreEqual(true, joueur.Contain("Mot"));
+        }
+
+        [TestMethod()]
+        public void toStringTest()
+        {
+            Joueur joueur = new Joueur("Test");
+            string s = joueur.toString();
+            Assert.AreEqual("Le score de Test est de 0, grâce aux mots cités suivent :\n", s);
+        }
+    }
+}
